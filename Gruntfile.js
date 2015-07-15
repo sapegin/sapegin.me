dc971d6d50d962370169ba67d8df376c66f92770
Artem Sapegin <artem@sapegin.ru>
2015-07-15 23:01:33 +0200

    Add image minification to build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,6 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.registerTask('default', ['fontoptim', 'styles', 'concat', 'uglify']);
+	grunt.registerTask('default', ['fontoptim', 'styles', 'concat', 'uglify', 'images']);
 	grunt.registerTask('deploy', ['default']);
 };
