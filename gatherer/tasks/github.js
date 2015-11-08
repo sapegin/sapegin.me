1b7bb24f1344b477c47fde01b5111d913f04ab16
Artem Sapegin <artem@sapegin.ru>
2015-11-08 23:13:49 +0100

    Parse GitHub contributions SVG because GitHub API now does not provide enough number of events.

--- gatherer/tasks/github.js
+++ gatherer/tasks/github.js
@@ -1,102 +1,90 @@
 /**
  * GitHub commits stats:
- * - Commit number for last N weeks
- * - Total commits number in N weeks
- * - Number of unique repos in N weeks
+ * - Contributions number for last N weeks
+ * - Total contributions number in N weeks
  *
+ * Required options: githubUser
+ * 
  * @author Artem Sapegin
- * @copyright 2012 Artem Sapegin (sapegin.me)
+ * @copyright 2015 Artem Sapegin (sapegin.me)
  * @license MIT
  */
 
 var WEEK = 1000 * 60 * 60 * 24 * 7;
-var MAX_PAGE = 10;
 
-var GitHub = require('github');
+var request = require('request');
+var parser = require('xml2json');
 
 exports.task = function(options, callback) {
 	'use strict';
 
-	var github = new GitHub({ version: '3.0.0' }),
-		now = new Date().getTime(),
-		dateRange = WEEK * options.weeksCount,
-		oldestDate = now - dateRange,
-		pushes = [];
-
-
-	function get(page) {
-		page = page || 1;
-		github.events.getFromUser({
-			user: options.githubUser,
-			page: page
-		}, function(err, events) {
-			if (err) {
-				console.log(err.message);
-				callback(null);
-			}
-			else {
-				var lastEvent = events[events.length-1];
-				if (lastEvent) {
-					pushes = pushes.concat(parseResponse(events));
-
-					var lastEventDate = new Date(lastEvent.created_at).getTime();
-					if (lastEventDate > oldestDate && page < MAX_PAGE) {
-						get(page + 1);
-						return;
-					}
-				}
-				parsePushes(pushes);
-			}
-		});
+	function get() {
+		request('https://github.com/users/' + options.githubUser + '/contributions', parseResponse);
 	}
 
-	function parseResponse(events) {
-		var pushes = [];
-		events.forEach(function(event) {
-			if (event.type !== 'PushEvent') return;
-			if (!event.payload || !event.payload.commits) return;
+	function parseResponse(err, response, xml) {
+		if (err || response.statusCode !== 200) {
+			console.log('HTTP request to GitHub failed', err);
+			callback(null);
+			return;
+		}
 
-			pushes.push({
-				date: event.created_at,
-				repo: event.repo.name,
-				commits: event.payload.commits.length
-			});
+		var json = parser.toJson(xml, {
+			object: true,
+			coerce: true
 		});
-		return pushes;
+
+		parseContributions(json.svg.g.g);
 	}
 
-	function parsePushes(pushes) {
-		var weeks = {},
-			totalCommits = 0,
-			repos = {},
-			uiqueRepos = 0;
+	function parseContributions(contributions) {	
+		var contributionsByDay = [];
+		contributions.forEach(function(json) {
+			if (json.rect.class === 'day') {
+				contributionsByDay.push({
+					date: json.rect['data-date'],
+					count: json.rect['data-count']
+				});
+			}
+			else if (Array.isArray(json.rect)) {
+				Array.prototype.push.apply(contributionsByDay, json.rect.map(function(json) {
+					return {
+						date: json['data-date'],
+						count: json['data-count']
+					};
+				}));
+			}
+		});
 
-		pushes.forEach(function(push) {
-			var date = new Date(push.date).getTime(),
+		contributionsByDay = contributionsByDay.reverse();
+		
+		var now = new Date().getTime();
+		var weeks = {};
+
+		contributionsByDay.forEach(function(day) {
+			var date = new Date(day.date).getTime(),
 				weekNum = Math.floor((now - date) / WEEK);
 
 			if (!weeks[weekNum]) {
 				weeks[weekNum] = 0;
 			}
-			weeks[weekNum] += push.commits;
-			totalCommits += push.commits;
-
-			if (!repos[push.repo]) {
-				repos[push.repo] = true;
-				uiqueRepos++;
-			}
+			weeks[weekNum] += day.count;
 		});
 
-		var dataset = { commits: [], totalCommits: totalCommits, uiqueRepos: uiqueRepos };
+		var contributionsByWeek = [];
+		var totalContributions = 0;
 		for (var weekNum = 0; weekNum <= options.weeksCount; weekNum++) {
-			var commits = weeks[weekNum] || 0;
-			dataset.commits.push(commits);
+			var contributionsInWeek = weeks[weekNum] || 0;
+			contributionsByWeek.push(contributionsInWeek);
+			totalContributions += contributionsInWeek;
 		}
-		dataset.commits = dataset.commits.reverse();
+		contributionsByWeek = contributionsByWeek.reverse();
 
-		callback(dataset);
+		callback({
+			commits: contributionsByWeek,
+			totalCommits: totalContributions
+		});
 	}
-
 
 	get();
 };
