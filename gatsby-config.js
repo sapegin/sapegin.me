b2418d9ecacd7f9fe70d86ccdf48fb02212a29c6
Artem Sapegin <asapegin@wayfair.com>
2019-03-24 13:11:17 +0100

    Add Fathom stats

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,12 @@ module.exports = {
 		},
 		'gatsby-transformer-remark',
 		'gatsby-plugin-netlify',
+		{
+			resolve: 'gatsby-plugin-fathom',
+			options: {
+				trackingUrl: 'stats.sapegin.me',
+				siteId: 'DUNEQ',
+			},
+		},
 	],
 };
