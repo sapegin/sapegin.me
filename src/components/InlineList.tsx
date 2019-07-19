3f3582fd2dee76c490e9a47a7cc2199268fe48e2
Artem Sapegin <asapegin@wayfair.com>
2019-07-19 09:36:34 +0200

    Make inline list responsive

--- src/components/InlineList.tsx
+++ src/components/InlineList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Text } from 'tamia';
 import { Link } from 'tamia-gatsby-link';
+import InlineListItem from './InlineListItem';
 
 type Item = {
 	title: string;
@@ -12,29 +12,13 @@ type Props = {
 	items: Item[];
 };
 
-const Item = styled.li`
-	display: inline;
-	& + &::before {
-		/* Draw a bullet with CSS because pseudo content is read by
-		   screen readers */
-		content: '';
-		display: inline-block;
-		vertical-align: middle;
-		margin: 0 1ch;
-		width: 3px;
-		height: 3px;
-		border-radius: 50%;
-		background-color: currentColor;
-	}
-`;
-
 export default function InlineList({ items }: Props) {
 	return (
 		<Text as="ul" variant="small">
 			{items.map(item => (
-				<Item key={item.link}>
+				<InlineListItem as="li" key={item.link}>
 					<Link href={item.link}>{item.title}</Link>
-				</Item>
+				</InlineListItem>
 			))}
 		</Text>
 	);
