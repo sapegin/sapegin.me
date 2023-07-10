ff4c3d8284cda98b2a4ad718fed078bd18f06407
Artem Sapegin <artem@sapegin.ru>
2023-07-10 08:53:34 +0200

    Add sitemap 🗺️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import remarkTips from './src/util/remark/remarkTips';
 import theme from './src/styles/shiki-theme.json';
+import { SITE_URL } from './src/constants';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+	site: SITE_URL,
+	integrations: [react(), sitemap()],
 	vite: {
 		plugins: [vanillaExtractPlugin()],
 	},
