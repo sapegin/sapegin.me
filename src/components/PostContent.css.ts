f29fcef9ad6a2f6951d6b9ad197dfe2fa01a4c2a
Artem Sapegin <artem@sapegin.ru>
2023-07-15 09:32:14 +0200

    Prettier list styles

--- src/components/PostContent.css.ts
+++ src/components/PostContent.css.ts
@@ -1,5 +1,7 @@
 import { style, globalStyle } from '@vanilla-extract/css';
 import { sprinkles } from '../styles/sprinkles.css';
+import { vars } from '../styles/theme.css';
+import { mediaQueries } from '../tamia/primitives/theme';
 
 export const postContent = style([
 	sprinkles({
@@ -9,6 +11,27 @@ export const postContent = style([
 	{},
 ]);
 
+globalStyle(`${postContent} ol`, {
+	marginLeft: 0,
+});
+
+globalStyle(`${postContent} ul > li`, {
+	paddingLeft: '1em',
+});
+globalStyle(`${postContent} ul > li::before`, {
+	content: '',
+	top: '.5em',
+	left: '.1em',
+	width: '.3em',
+	height: '.3em',
+	backgroundColor: vars.colors.base,
+	'@media': {
+		[mediaQueries.tablet]: {
+			top: '.6em',
+		},
+	},
+});
+
 globalStyle(`${postContent} hr`, {
 	textAlign: 'center',
 	backgroundColor: 'transparent',
@@ -16,7 +39,6 @@ globalStyle(`${postContent} hr`, {
 	// Make top and bottom margins more or less the same
 	marginBlock: '2.5rem',
 });
-
 globalStyle(`${postContent} hr::after`, {
 	content: '···',
 	letterSpacing: '0.7em',
