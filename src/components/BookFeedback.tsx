ba9a1d7ba241c899aa8cd46d8d46b47ccf2c09f1
Artem Sapegin <artem@sapegin.ru>
2023-07-16 09:49:32 +0200

    Tweak book feedback links

--- src/components/BookFeedback.tsx
+++ src/components/BookFeedback.tsx
@@ -8,12 +8,12 @@ type Props = {
 export function BookFeedback({ children }: Props) {
 	return (
 		<Text variant="intro">
-			{children} <Link href="mailto:artem@sapegin.ru">artem@sapegin.ru</Link>,{' '}
-			<Link href="https://twitter.com/iamsapegin">@iamsapegin</Link>,{' '}
+			{children} write me at{' '}
+			<Link href="mailto:artem@sapegin.ru">artem@sapegin.ru</Link>,{' '}
 			<Link href="https://mastodon.cloud/@sapegin" rel="me">
 				@sapegin@mastodon.cloud
 			</Link>
-			, or{' '}
+			, <Link href="https://twitter.com/iamsapegin">@iamsapegin</Link>, or{' '}
 			<Link href="https://github.com/sapegin/washingcode-book/issues">
 				open an issue
 			</Link>
