7d9f96440f69f7085346bd490a4db3cd90df79a2
Artem Sapegin <artem@sapegin.ru>
2023-07-25 07:40:24 +0200

    Add image lazy loading

--- src/tamia/components/Image.tsx
+++ src/tamia/components/Image.tsx
@@ -4,5 +4,5 @@ import { image } from './Image.css';
  * Responsive image.
  */
 export function Image(props: JSX.IntrinsicElements['img']) {
-	return <img className={image} {...props} />;
+	return <img loading="lazy" className={image} {...props} />;
 }
