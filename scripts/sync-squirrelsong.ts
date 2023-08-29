5993b37675a6450b9ffd99ed3f92f4368e17cf68
Artem Sapegin <artem@sapegin.ru>
2023-08-29 08:26:38 +0200

    Fix build

--- scripts/sync-squirrelsong.ts
+++ scripts/sync-squirrelsong.ts
@@ -1,5 +1,6 @@
 // Get themes from the Squirrelsong repo
 
+import fs from 'fs-extra';
 import { execSync } from 'child_process';
 
 const REPO_TAR_GZ =
@@ -8,5 +9,10 @@ const REPO_TAR_GZ =
 console.log('[SQRLSNG] Downloading themes...');
 
 execSync(`curl "${REPO_TAR_GZ}" | tar xz`);
+
+console.log('[SQRLSNG] Cleaning up...');
+
+// Remove samples not to confuse Astro checker
+fs.removeSync('squirrelsong-master/sample');
 
 console.log('[SQRLSNG] Done 🦜');
