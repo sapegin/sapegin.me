a97fcff24f5f5fd11d704663f69470c4baab54ba
Artem Sapegin <artem@sapegin.ru>
2023-08-30 08:18:15 +0200

    Migrate newsletter to Substack 📯

--- src/components/SubscriptionForm.tsx
+++ src/components/SubscriptionForm.tsx
@@ -2,25 +2,11 @@ import { Box, VisuallyHidden, Input, Button, InputGroup } from '.';
 
 export function SubscriptionForm() {
 	return (
-		<form method="post" action="https://tinyletter.com/sapegin" target="_blank">
-			<InputGroup>
-				<Box as="label" width="100%">
-					<VisuallyHidden>Your email</VisuallyHidden>
-					<Input
-						name="email"
-						type="email"
-						required
-						autoComplete="home email"
-						autoCapitalize="off"
-						autoCorrect="off"
-						placeholder="Your email"
-						defaultValue=""
-					/>
-				</Box>
-				<Box flexShrink={0}>
-					<Button type="submit">Subscribe</Button>
-				</Box>
-			</InputGroup>
-		</form>
+		<iframe
+			src="https://sapegin.substack.com/embed"
+			width="100%"
+			height="320"
+			loading="lazy"
+		/>
 	);
 }
