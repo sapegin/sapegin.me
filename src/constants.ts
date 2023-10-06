81f97f2f5794415c0ca914a40ca40c83587d4008
Artem Sapegin <artem@sapegin.ru>
2023-10-06 09:54:48 +0200

    Fix social images

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const SITE_HOST = 'sapegin.me';
 export const SITE_URL = `https://${SITE_HOST}`;
+export const SITE_AUTHOR = 'Artem Sapegin';
 export const SITE_TITLE = 'Artem Sapegin';
 export const SITE_DESCRIPTION =
 	'Coffee first frontend engineer, photographer, leathercrafter, food and coffee enthusiast.';
---
+++ src/pages/cards/[...route].ts
@@ -0,0 +1,42 @@
+import { OGImageRoute } from 'astro-og-canvas';
+import hexRgb from 'hex-rgb';
+import { theme } from '../../styles/theme.css';
+import { SITE_AUTHOR } from '../../constants';
+
+const hexToRgb = (hex: string): [number, number, number] => {
+	const { red, green, blue } = hexRgb(hex);
+	return [red, green, blue];
+};
+
+export const { getStaticPaths, GET } = OGImageRoute({
+	param: 'route',
+	pages: await import.meta.glob('/src/content/**/*.md', { eager: true }),
+	getImageOptions: (path, page) => ({
+		title: page.frontmatter.title,
+		description: `By ${SITE_AUTHOR}`,
+		bgGradient: [hexToRgb(theme.colors.bg), hexToRgb(theme.colors.bg)],
+		padding: 48,
+		border: {
+			color: hexToRgb(theme.colors.hover),
+			width: 24,
+			side: 'block-start',
+		},
+		font: {
+			title: {
+				color: hexToRgb(theme.colors.base),
+				size: 80,
+				lineHeight: Number(theme.lineHeights.heading),
+				families: ['Roboto Mono', 'sans-serif'],
+			},
+			description: {
+				color: hexToRgb(theme.colors.base),
+				size: 40,
+				lineHeight: Number(theme.lineHeights.base),
+				families: ['Roboto Mono', 'sans-serif'],
+			},
+		},
+		fonts: [
+			'https://api.fontsource.org/v1/fonts/roboto-mono/latin-400-normal.ttf',
+		],
+	}),
+});
