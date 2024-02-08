1de038e058902af83043f11af924c144de9becaf
Artem Sapegin <artem@sapegin.ru>
2024-02-08 12:12:51 +0100

    Upgrade Astro to v4

--- src/util/highlightCode.ts
+++ src/util/highlightCode.ts
@@ -1,14 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import stripJsonComments from 'strip-json-comments';
-import shiki from 'shiki';
-import langTSX from 'shiki/languages/tsx.tmLanguage.json';
-import langMarkdown from 'shiki/languages/markdown.tmLanguage.json';
-import langCss from 'shiki/languages/css.tmLanguage.json';
-import langHtml from 'shiki/languages/html.tmLanguage.json';
-import langJson from 'shiki/languages/json.tmLanguage.json';
-import langPhp from 'shiki/languages/php.tmLanguage.json';
-import langPython from 'shiki/languages/python.tmLanguage.json';
+import { getHighlighter } from 'shikiji';
 
 const THEMES_DIR = 'squirrelsong-master';
 
@@ -47,53 +40,9 @@ export type CodeLang =
 	| 'python';
 export type CodeTheme = 'light' | 'dark';
 
-const highlighter = await shiki.getHighlighter({
-	// @ts-ignore
+const highlighter = await getHighlighter({
 	themes: [lightTheme, darkTheme],
-	langs: [
-		{
-			id: 'tsx',
-			scopeName: langTSX.scopeName,
-			// @ts-ignore
-			grammar: langTSX,
-		},
-		{
-			id: 'markdown',
-			scopeName: langMarkdown.scopeName,
-			// @ts-ignore
-			grammar: langMarkdown,
-		},
-		{
-			id: 'css',
-			scopeName: langCss.scopeName,
-			// @ts-ignore
-			grammar: langCss,
-		},
-		{
-			id: 'html',
-			scopeName: langHtml.scopeName,
-			// @ts-ignore
-			grammar: langHtml,
-		},
-		{
-			id: 'json',
-			scopeName: langJson.scopeName,
-			// @ts-ignore
-			grammar: langJson,
-		},
-		{
-			id: 'php',
-			scopeName: langPhp.scopeName,
-			// @ts-ignore
-			grammar: langPhp,
-		},
-		{
-			id: 'python',
-			scopeName: langPython.scopeName,
-			// @ts-ignore
-			grammar: langPython,
-		},
-	],
+	langs: ['tsx', 'markdown', 'css', 'html', 'json', 'php', 'python'],
 });
 
 export function highlightCode(code: string, lang: CodeLang, theme: CodeTheme) {
