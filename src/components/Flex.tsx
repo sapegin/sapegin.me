4f8a21fad60964292ca26e326d183ea7b5cb6c74
Artem Sapegin <artem@sapegin.ru>
2024-04-01 10:25:46 +0200

    Actually flex pattern

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,8 +1,9 @@
 import { type ElementType } from 'react';
+import { flex } from '../../styled-system/patterns/flex';
 import { createBox, type BoxProps } from './Box';
 
-export type FlexProps<C extends ElementType> = BoxProps<C>;
+export type FlexProps<C extends ElementType> = Omit<BoxProps<C>, 'className'>;
 
 export function Flex<C extends ElementType>(props: FlexProps<C>) {
-	return createBox(props);
+	return createBox({ ...props, className: flex() });
 }
