7d3e5c579e4de3d2f38c36e0b802818e2664619d
Artem Sapegin <artem@sapegin.ru>
2024-04-01 09:39:05 +0200

    Split patterns into separate files

---
+++ src/tamia-panda/patterns/index.ts
@@ -0,0 +1,20 @@
+import { box } from './box';
+import { flex } from './flex';
+import { grid } from './grid';
+import { link } from './link';
+import { quotedLink } from './quotedLink';
+import { stack } from './stack';
+import { textContent } from './textContent';
+import { visuallyHidden } from './visuallyHidden';
+
+// Patterns
+export const patterns = {
+	box,
+	flex,
+	grid,
+	link,
+	quotedLink,
+	stack,
+	textContent,
+	visuallyHidden,
+};
