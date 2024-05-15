db4eb66a8094c970f415e06790d9724d4c99e1a0
Artem Sapegin <artem@sapegin.ru>
2024-05-15 17:03:10 +0200

    Update Squirrelsong page

--- src/templates/SquirrelsongPage.tsx
+++ src/templates/SquirrelsongPage.tsx
@@ -106,75 +106,87 @@ function Features({ codes }: Pick<Props, 'codes'>) {
 			<VisuallyHidden as="h2">Features</VisuallyHidden>
 			<Stack gap="s">
 				<Heading level={3}>Low contrast with great readability</Heading>
-				<TextTypo>
-					Most themes have very high contrast and use bright colors which makes
-					them tiring for the eyes, especially for highly sensitive people.
-					Squirrelsong themes are low-contrast but still provide enough color
-					and style variety to distinguish various elements of the code and
-					avoid long chains of code printed in the same style.
-				</TextTypo>
-				<CodeSpread codes={codes} name="intro" />
+				<Stack gap="m">
+					<TextTypo>
+						Most themes have very high contrast and bright colors which makes
+						them tiring for the eyes, especially for highly sensitive people.
+						Squirrelsong themes are low-contrast and use soft colors, but still
+						provide enough color and style variety to distinguish various
+						elements of the code and avoid long chains of code printed in the
+						same style.
+					</TextTypo>
+					<CodeSpread codes={codes} name="intro" />
+				</Stack>
 			</Stack>
 			<Stack gap="s">
 				<Heading level={3}>Made for web developers</Heading>
-				<TextTypo>
-					Carefully crafted syntax highlighting for JavaScript, TypeScript,
-					HTML, CSS, Sass, styled-components, Markdown, JSON, XML, React, and
-					Astro. Also, works well with PHP, Python, Ruby, Rust, Java, Swift,
-					etc.
-				</TextTypo>
-				<CodeSpreadSupreme codes={codes} names={['html', 'css', 'json']} />
+				<Stack gap="m">
+					<TextTypo>
+						Carefully crafted syntax highlighting for JavaScript, TypeScript,
+						HTML, CSS, Sass, styled-components, Markdown, JSON, XML, React, and
+						Astro. Also, works well with PHP, Python, Ruby, Rust, Java, Swift,
+						etc.
+					</TextTypo>
+					<CodeSpreadSupreme codes={codes} names={['html', 'css', 'json']} />
+				</Stack>
 			</Stack>
 			<Stack gap="s">
 				<Heading level={3}>Monochrome Markdown styles</Heading>
-				<TextTypo>
-					Inspired by iA Writer, Markdown is styled in shades of gray to avoid
-					distractions while writing your next blog post, documentation, or
-					book.
-				</TextTypo>
+				<Stack gap="m">
+					<TextTypo>
+						Inspired by iA Writer, Markdown is styled in shades of gray to avoid
+						distractions while writing your next blog post, documentation, or
+						book.
+					</TextTypo>
+				</Stack>
 				<CodeSpread codes={codes} name="markdown" />
 			</Stack>
 			<Stack gap="s">
 				<Heading level={3}>
 					Consistent highlighting of different programming languages
 				</Heading>
-				<TextTypo>
-					Colors and styles are consistent among different programming
-					languages: for example, <code>this</code> in TypeScript is styled the
-					same way as <code>$this</code> in PHP and <code>self</code> in Python
-					or Rust. HTML looks the same as JSX in React or markup in Astro
-					components.
-				</TextTypo>
-				<CodeSpreadSupreme
-					codes={codes}
-					names={['typescript', 'php', 'python']}
-				/>
+				<Stack gap="m">
+					<TextTypo>
+						Colors and styles are consistent among different programming
+						languages: for example, <code>this</code> in TypeScript is styled
+						the same way as <code>$this</code> in PHP and <code>self</code> in
+						Python or Rust. HTML looks the same as JSX in React or markup in
+						Astro components.
+					</TextTypo>
+					<CodeSpreadSupreme
+						codes={codes}
+						names={['typescript', 'php', 'python']}
+					/>
+				</Stack>
 			</Stack>
 			<Stack gap="s">
 				<Heading level={3}>Non-distracting UI</Heading>
-				<TextTypo>
-					Custom UI for Visual Studio Code, JetBrains IDEs, and other apps
-					without oversaturated bright colors to keep you focused on your code
-					instead of being distracted and overwhelmed.
-				</TextTypo>
-				<Grid auto="wide" gap="m">
-					<Box objectFit="cover" overflow="hidden">
-						<img
-							src="/images/squirrelsong-vscode.png"
-							width={652}
-							height={459}
-							alt="Squirrelsong light theme for Visual Studio Code"
-						/>
-					</Box>
-					<Box objectFit="cover" overflow="hidden">
-						<img
-							src="/images/squirrelsong-jetbrains.webp"
-							width={652}
-							height={459}
-							alt="Squirrelsong light theme for JetBrains"
-						/>
-					</Box>
-				</Grid>
+				<Stack gap="m">
+					<TextTypo>
+						Custom UI for Visual Studio Code, and other apps without
+						oversaturated bright colors to keep you focused on your code instead
+						of being distracted and overwhelmed. Compare it to the default
+						Visual Studio Code theme.
+					</TextTypo>
+					<Grid auto="wide" gap="m">
+						<Box objectFit="cover" overflow="hidden">
+							<img
+								src="/images/squirrelsong/squirrelsong-vscode.avif"
+								width={593}
+								height={459}
+								alt="Squirrelsong light theme for Visual Studio Code"
+							/>
+						</Box>
+						<Box objectFit="cover" overflow="hidden">
+							<img
+								src="/images/squirrelsong/squirrelsong-vscode-default.avif"
+								width={593}
+								height={459}
+								alt="Default light theme for Visual Studio Code"
+							/>
+						</Box>
+					</Grid>
+				</Stack>
 			</Stack>
 		</Stack>
 	);
@@ -217,11 +229,17 @@ export function SquirrelsongPage({ url, codes }: Props) {
 			<main>
 				<Stack gap="l">
 					<Hero />
+					<Text variant="intro">
+						A low-contrast, non-distracting, and neurodiverse-friendly theme
+						that is comfortable for all-day coding without sensory
+						overstimulation. It’s perfect for developers with ADHD, autism, or
+						highly sensitive people (HSP).
+					</Text>
 					<Features codes={codes} />
 					<Installation />
 					<About>
 						I created the first version of this theme in 2016, and since then
-						used it every day at work and for personal projects. Seven years
+						use it every day at work and for personal projects. Seven years
 						later, I rebuilt it almost from scratch and improved many things.
 					</About>
 					<Stack gap="m">
