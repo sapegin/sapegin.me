c16a41140cd9d97b8f157fb3d5dcbdb7139d6e3a
Artem Sapegin <artem@sapegin.me>
2026-04-13 12:32:28 +0200

    Clean up

--- sites/tacohuaco/src/shared.ts
+++
@@ -1,7 +0,0 @@
-// TODO: Merge with constants.ts
-export const SITE_URL = 'https://tacohuaco.co';
-export const SITE_TITLE = 'Tacohuaco';
-export const SITE_INDEX_TITLE =
-	'Tacohuaco: favorite recipes of a Mexican-Russian couple';
-export const SITE_DESCRIPTION =
-	'Favorite recipes of a Mexican-Russian couple: low gluten, low sugar, maximum flavor and pleasure!';
